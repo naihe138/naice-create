f665f29ff12ea4dc94b9aa71327b4901fd07e98d
naihe138 <370215230@qq.com>
2022-04-16 22:47:39 +0800

    feat: 新增库的模板

--- src/config.ts
+++ src/config.ts
@@ -2,5 +2,5 @@ export const templateConfig = {
   h5: 'git@github.com:naihe138/vite-ts-template.git',
   react: 'git@github.com:naihe138/vite-react-templage.git',
   express: 'git@github.com:naihe138/ts-express-template.git',
-  lib: 'git@github.com:naihe138/ts-express-template.git'
+  lib: 'git@github.com:naihe138/ts-lib-template.git'
 }
